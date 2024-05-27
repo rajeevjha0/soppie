a2f2bc67e4df03a20aeea9a2ce9bcabf7510afc7
Rajeev Kumar Jha <rajeev.amr0606@gmail.com>
2024-05-27 16:10:42 +0530

    fnklasc

--- src/components/Newproducts.jsx
+++ src/components/Newproducts.jsx
@@ -63,10 +63,10 @@ export default function ProductList() { // Added a function name "ProductList"
     ];
 
     return (
-        <div className="container pb-[80px]">
+        <div className="container pb-[80px] px-[30px] mx-auto">
             <h2 className="font-medium text-5xl	text-center pb-4">New Products</h2>
             <p className="text-center mb-[25px]">Find your style. Fall fashion 20xx</p>
-            <div className="grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-10 xl:gap-x-0 xl:gap-y-10">
+            <div className="grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-10 xl:gap-x-0 xl:gap-y-10 ml-[20px]">
                 {products.map((item, index) => (
                     <ProductCard // Changed from Productcard to ProductCard
                         key={index}
